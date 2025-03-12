266c0adbbd0468faa1deb268e4c1bf80c6afec62
As-bu <asbu666@pm.me>
2025-03-12 21:36:04 +0100

    B: optimization, adding sprites

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ from constants import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def game_loop():
-	loop = 0
-	while loop == 0:
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0 
+
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
 		screen.fill(000, rect = None, special_flags = 0)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+		print(dt)
 
 def main():
-	print("Starting Asteroids!", 
-		f"Screen width: {SCREEN_WIDTH}", 
-		f"Screen height: {SCREEN_HEIGHT}"
-	)
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
 	game_loop()
 
 if __name__ == "__main__":
